cfd6a96753c2b7c8b8cbc3db87cf1c07e1d54a06
Luke Schlangen <lukeschlangen@gmail.com>
2017-02-01 21:17:25 -0600

    Not working after signing back in

--- app.js
+++ app.js
@@ -1,71 +1,48 @@
 // Initialize Firebase
-
-// You will need to paste your web code here
-// var config = {
-//     apiKey: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXxxx",
-//     authDomain: "XXXXXXXXXX.firebaseapp.com",
-//     databaseURL: "https://XXXXXXX.firebaseio.com",
-//     storageBucket: "XXXXXXXX.appspot.com",
-// };
-// firebase.initializeApp(config);
-
-function updateState() {
-    var header = document.getElementById('header');
-    var dbRef = firebase.database().ref().child('header');
-    dbRef.on('value', function(snap) {
-        header.innerText = snap.val();
-    });
-}
+var config = {
+    apiKey: "AIzaSyBFUzWta0A7n9ZYp9Piv25RAAtPKZFj-8Y",
+    authDomain: "prime-fun.firebaseapp.com",
+    databaseURL: "https://prime-fun.firebaseio.com",
+    storageBucket: "prime-fun.appspot.com",
+};
+firebase.initializeApp(config);
 
 // Authentication Logic
-var emailText = document.getElementById('emailText');
-var passwordText = document.getElementById('passwordText');
-var loginButton = document.getElementById('loginButton');
-var signUpButton = document.getElementById('signUpButton');
+var signInButton = document.getElementById('signInButton');
 var signOutButton = document.getElementById('signOutButton');
+var header = document.getElementById('header');
+var playground = document.getElementById('playground');
+var provider = new firebase.auth.GoogleAuthProvider();
 
-loginButton.addEventListener('click', function(e){
-    var email = emailText.value;
-    var password = passwordText.value;
-    var auth = firebase.auth();
+var dbRef = firebase.database().ref().child('header');
+dbRef.on('value', function (snap) {
+    header.innerText = snap.val();
+});
 
-    var promise = auth.signInWithEmailAndPassword(email, password);
-    promise.catch(function(e){
-        console.log(e.message);
-    })
-})
+var playgroundRef = firebase.database().ref().child('playground');
+playgroundRef.on('value', function (snap) {
+    playground.innerText = snap.val();
+});
 
-signUpButton.addEventListener('click', function(e){
-    var email = emailText.value;
-    var password = passwordText.value;
-    var auth = firebase.auth();
+signInButton.addEventListener('click', function (e) {
+    firebase.auth().signInWithPopup(provider);
+});
 
-    var promise = auth.createUserWithEmailAndPassword(email, password);
-    promise.catch(function(e){
-        console.log(e.message);
-    })
-})
-
-firebase.auth().onAuthStateChanged(function(firebaseUser){
-    if(firebaseUser){
-        updateState();
-        console.log(firebaseUser);
+firebase.auth().onAuthStateChanged(function (firebaseUser) {
+    if (firebaseUser) {
+        console.log('The current user is: ', firebaseUser);
     } else {
+        header.innerText = 'Log in using Google!';
+        playground.innerText = '';
         console.log('not logged in');
     }
 });
 
-signOutButton.addEventListener('click', function(e){
+signOutButton.addEventListener('click', function (e) {
     firebase.auth().signOut();
 });
 
-var playground = document.getElementById('playground');
-var playgroundRef = firebase.database().ref().child('playground');
-playgroundRef.on('value', function(snap) {
-    playground.innerText = snap.val();
-});
-
-playground.addEventListener('keyup', function(){
+playground.addEventListener('keyup', function () {
     playgroundRef.set(playground.value);
-})
+});
 
